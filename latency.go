74e6d2be8142136ed32006a7b52b193f93c2b407
Yves Junqueira <yves.junqueira@gmail.com>
2014-04-13 20:58:55 +0200

    Move graph plotting to a separate library, pt 2

--- latency.go
+++ latency.go
@@ -2,13 +2,8 @@
 package latency
 
 import (
-	"fmt"
 	"math"
-	"os"
 	"time"
-
-	"code.google.com/p/plotinum/plot"
-	"code.google.com/p/plotinum/plotter"
 )
 
 // Histogram can track latency distributions.
@@ -43,36 +38,4 @@ func (h *Histogram) Record(d time.Duration) {
 		}
 	}
 	h.Buckets[bucket]++
-}
-
-// Plot saves an image of the latency histogram to filePath. The extension of filePath defines
-// the format to be used - png, svg, etc.
-// TODO(nictuku): Move to a sub-package to avoid import bloat.
-func (h *Histogram) Plot(filePath string) error {
-	count := len(h.Buckets)
-	xys := make(plotter.XYs, count)
-
-	for bucket, freq := range h.Buckets {
-		xys[bucket].X = float64(bucket)
-		xys[bucket].Y = float64(freq)
-	}
-
-	p, err := plot.New()
-	if err != nil {
-		return fmt.Errorf("error generating plot: %v", err)
-	}
-	p.Title.Text = "Latency histogram"
-	p.X.Label.Text = fmt.Sprintf("Latency (%v resolution)", h.Resolution)
-	p.Y.Label.Text = "Frequency"
-
-	hh, err := plotter.NewHistogram(xys, count)
-	if err != nil {
-		return fmt.Errorf("error generating histogram: %v", err)
-
-	}
-	p.Add(hh)
-
-	// Save the plot to a file. Units in inches (one inch == 72 points).
-	fmt.Fprintf(os.Stderr, "Saving latency histogram to %v\n", filePath)
-	return p.Save(8, 6, filePath)
 }
